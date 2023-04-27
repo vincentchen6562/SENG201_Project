82e2d2f3630b338b7f90bbcd4cbaa5423dda91bb
kelsodumez <kelsodumez@gmail.com>
2023-04-27 17:02:20 +1200

    some work on difficulty

--- Difficulty.java
+++ Difficulty.java
@@ -1,3 +1,11 @@
 public class Difficulty {
-    
+    private int difficulty;
+
+    public void setDifficulty(int new_difficulty){
+        difficulty = new_difficulty;
+    }
+
+    public int getDifficulty(){
+        return difficulty;
+    }
 }
