590d062c3e2f64ceed85506ead8a67786beefc41
kelsodumez <17232@burnside.school.nz>
2023-04-28 13:10:15 +1200

    work on game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,34 @@
 public class Game {
-    
+    private int seasonNumber;
+    private int weekNumber;
+
+    public Game(int seasonNumber, int weekNumber) {
+        this.seasonNumber = seasonNumber;
+        this.weekNumber = weekNumber;
+    }
+
+    public void setSeason(int seasonNum) {
+        seasonNumber = seasonNum;
+    }
+
+    public void incrementSeason() {
+        seasonNumber += 1;
+    }
+
+    public int getSeason() {
+        return seasonNumber;
+    }
+
+    public void setWeekN(int week) {
+        weekNumber = week;
+    }
+
+    public void incrementWeek() {
+        weekNumber += 1;
+    }
+
+    public int getWeek() {
+        return weekNumber;
+    }
+
 }
