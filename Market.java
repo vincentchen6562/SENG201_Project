59d892b786aec26025f6b0d9f87cd7980571d6fd
JustCarbon <48465004+vincentchen6562@users.noreply.github.com>
2023-05-12 21:43:16 +1200

    Change difficulty to string

--- Market.java
+++ Market.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class Market {
-    
+    private ArrayList<Item> allItems;
+    private ArrayList<Athlete> allAthletes;
+
+    public void generateItemShop(Difficulty difficulty, int seasonNumber) {
+        // potentially need a stream function to generate items
+    }
+
+    public void generateAthleteShop(Difficulty difficulty, int seasonNumber) {
+        // use a similar method as above to generate athlete objects
+    }
+
+    public void recruitAthlete(Athlete athlete, )
+
 }
