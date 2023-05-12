b06f661a95161467e33a1de9400042c917740c4c
JustCarbon <48465004+vincentchen6562@users.noreply.github.com>
2023-05-12 22:25:05 +1200

    Started on the Match class

--- Athlete.java
+++ Athlete.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class Athlete extends Statistics {
     private String name;
@@ -6,16 +6,18 @@ public class Athlete extends Statistics {
     private String position;
     private int stamina;
     private boolean isInjured;
+    private int athleteSalary; // this needs to be zero for the initial 
 
 
 
-    public Athlete(String name, String nickName, String position, int stamina, boolean isInjured, int offence, int defence, int agility){
+    public Athlete(String name, String nickName, String position, int stamina, boolean isInjured, int offence, int defence, int agility, int price){
         super(offence, defence, agility);
         this.name = name;
         this.nickName = nickName;
         this.position = position;
         this.stamina = stamina;
         this.isInjured = isInjured;
+        athleteSalary = price;
 
     }
 
@@ -37,5 +39,9 @@ public class Athlete extends Statistics {
 
     public void setIsInjured(boolean state){
         isInjured = state;
+    }
+
+    public int getPrice() {
+        return athleteSalary;
     }
 }
--- Match.java
+++ Match.java
@@ -1,2 +1,13 @@
 public class Match {
+    private int scoreTotal;
+    private int priceMoney;
+    private int pointsWorth;
+
+    public Match(int totalScore, int reward, int points) {
+        scoreTotal = totalScore;
+        priceMoney = reward;
+        pointsWorth = points;
+    }
+
 }
+
