a4bd74060357226037e257a19e84fb48dbf2946e
VincentChen <vch6562@gmail.com>
2023-05-12 14:33:32 +1200

    changed nothing

--- Difficulty.java
+++ Difficulty.java
@@ -1,11 +1,11 @@
 public class Difficulty {
     private int difficulty;
 
-    public void setDifficulty(int new_difficulty){
+    public void setDifficulty(int new_difficulty) {
         difficulty = new_difficulty;
     }
 
-    public int getDifficulty(){
+    public int getDifficulty() {
         return difficulty;
     }
 }
