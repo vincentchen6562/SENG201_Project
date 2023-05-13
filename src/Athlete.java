15cf8c4310bebdeefb23d75c0d9839c7044038aa
VincentChen <vch6562@gmail.com>
2023-05-13 16:33:16 +1200

    moved personal tutorial project

--- src/Athlete.java
+++ src/Athlete.java
@@ -6,11 +6,10 @@ public class Athlete extends Statistics {
     private String position;
     private int stamina;
     private boolean isInjured;
-    private int athleteSalary; // this needs to be zero for the initial 
+    private int athleteSalary; // this needs to be zero for the initial
 
-
-
-    public Athlete(String name, String position, int stamina, boolean isInjured, int offence, int defence, int agility, int price){
+    public Athlete(String name, String position, int stamina, boolean isInjured, int offence, int defence, int agility,
+            int price) {
         super(offence, defence, agility);
         this.name = name;
         this.position = position;
@@ -20,23 +19,23 @@ public class Athlete extends Statistics {
 
     }
 
-    public String getFullName(){
+    public String getFullName() {
         return name + nickName;
     }
 
-    public String getPosition(){
+    public String getPosition() {
         return position;
     }
 
-    public int getStamina(){
+    public int getStamina() {
         return stamina;
     }
 
-    public void changeStamina(int amount){
+    public void changeStamina(int amount) {
         stamina += amount;
     }
 
-    public void setIsInjured(boolean state){ // need to set a random chance of this occurring
+    public void setIsInjured(boolean state) { // need to set a random chance of this occurring
         isInjured = state;
     }
 
@@ -46,5 +45,9 @@ public class Athlete extends Statistics {
 
     public void setNickName(String name) { // this allows the player to give an athlete a nickname
         nickName = name;
+    }
+
+    public void train(Athlete athlete) {
+
     }
 }
