17dea200157b761c5526dfdadfb667e6c0d964b4
JustCarbon <48465004+vincentchen6562@users.noreply.github.com>
2023-05-16 11:48:55 +1200

    made changes to market

--- src/Game.java
+++ src/Game.java
@@ -42,10 +42,12 @@ public class Game {
 // - Need to determine how the player can win or lose a game
 // - Need to update the UML diagram (classes added: Item, Coach, Equipment)
 // - Need to implement behaviours in this class's run function when a certain
-// button
+// button (a GameEnvironment class is needed to store the logic of the game)
 // is clicked, e.g. when the player purchases an athlete, the Team class adds an
 // athlete, the athlete is removed from the market, the bank variable in
 // inventory
 // is accordingly decreased.
+// - Need to restructure the packages since it is assessed
+// - Need to comment properly (for Javadoc and final assessment), this includes the GUI classes, REMEMBER: no commented out code
 
 // DONE
