350271ac472f5953e2e0f822ddaac22c0b67a50e
VincentChen <vch6562@gmail.com>
2023-05-22 14:07:21 +1200

    working on difficulty

--- src/Difficulty.java
+++ src/Difficulty.java
@@ -3,8 +3,8 @@ package src;
 public class Difficulty {
     private String difficulty; // difficulty is displayed as a string - "Normal" and "Hard"
 
-    public Difficulty(String new_difficulty) {
-        difficulty = new_difficulty;
+    public Difficulty(String newDifficulty) {
+        difficulty = newDifficulty;
     }
 
     public String getDifficulty() {
